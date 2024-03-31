726827422a19948cb61dc166ac02a027354c20cd
Naglis <naglis1134@gmail.com>
2024-04-01 00:42:44 +0300

    Language WIP

--- src/app/app.tranlastions.ts
+++ src/app/app.tranlastions.ts
@@ -3,4 +3,28 @@ export const TRANSLATIONS: { en: string; lt: string }[] = [
     en: 'Math test maker',
     lt: 'Matematikos testų gaminimo priemonė',
   },
+  {
+    en: 'This website generates HTML file with questionaire, where only one answer option is correct. The HTML file user then can answer questions and check the answers',
+    lt: 'Šis tinklapis sugeneruoja HTML failą su klausimais, kur tik vienas atsakymo variantas yra teisingas. HTML failo naudotojas tuomet gali atsakyti klausimus ir pasitikrinti atsakymus',
+  },
+  {
+    en: 'Enter Title',
+    lt: 'Įveskite pavadinimą',
+  },
+  {
+    en: 'Option',
+    lt: 'Pasirinkimas',
+  },
+  {
+    en: 'Question Text',
+    lt: 'Klausimo formuluotė',
+  },
+  {
+    en: 'Add question',
+    lt: 'Pridėti klausimą',
+  },
+  {
+    en: 'Remove last question',
+    lt: 'Pašalinti paskutinį klausimą',
+  },
 ];
